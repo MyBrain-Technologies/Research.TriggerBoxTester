b3ed190464636cdc17441cc24bdcd7d65f4be351
Michele Romani <michele.romani@mybraintech.com>
2023-04-27 01:42:46 +0200

    :art: improved clarity of UI; add messages to logbox; add logo and icons.

--- serial_sender.py
+++ serial_sender.py
@@ -1,7 +1,7 @@
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QComboBox, QTextEdit, QPushButton, QSlider, QHBoxLayout, \
     QVBoxLayout
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtGui import QPixmap, QIcon
 from serial.tools import list_ports
 import time
 import serial
@@ -12,20 +12,28 @@ class SerialSender(QWidget):
     def __init__(self):
         super().__init__()
 
-
         self.serial_port = None
-
 
         # create a label for the logo
         logo_label = QLabel(self)
         logo_label.setPixmap(QPixmap("assets/logo.png"))
-        logo_label.setScaledContents(True)
-        logo_label.setGeometry(10, 10, 100, 100)
+        logo_label.setScaledContents(False)
+        #logo_label.setGeometry(10, 10, 100, 100)
 
         # Combobox to select serial port
         self.serial_combo = QComboBox()
+        self.serial_combo.setMinimumWidth(120)
+        self.serial_combo.setMaximumHeight(40)
         self.serial_combo.currentTextChanged.connect(self.serial_port_selected)
         self.refresh_serial_ports()
+
+        # Add refresh button
+        self.refresh_button = QPushButton(QIcon("assets/refresh.png"), "", self)
+        self.refresh_button.setMaximumWidth(60)
+        self.refresh_button.setMaximumHeight(40)
+        self.refresh_button.setToolTip("Refresh serial ports list")
+        self.refresh_button.move(360, 45)
+        self.refresh_button.clicked.connect(self.refresh_serial_ports)
 
         # Textbox for inputting message to send
         self.message_box = QTextEdit()
@@ -49,19 +57,33 @@ class SerialSender(QWidget):
         self.log_box = QTextEdit()
         self.log_box.setReadOnly(True)
         self.log_box.setStyleSheet("background-color: black; color: white")
+        self.log_box.append("Welcome to the TriggerBox Tester, please select a serial port and start sending triggers.")
+        self.log_box.append("Please select a serial port and start sending triggers.")
+        self.log_box.append("Tip: write comma separated values in the text box above.")
+        self.log_box.repaint()
 
         # Layout
+        hbox_refresh = QHBoxLayout()
+        hbox_refresh.addWidget(self.serial_combo)
+        hbox_refresh.addWidget(self.refresh_button)
+        hbox_refresh.setAlignment(Qt.AlignLeft)
+
+        hbox_logo = QHBoxLayout()
+        hbox_logo.addWidget(logo_label)
+        hbox_logo.setAlignment(Qt.AlignCenter)
+
         vbox = QVBoxLayout()
-        vbox.addWidget(logo_label)
+        vbox.addLayout(hbox_logo)
         vbox.addWidget(QLabel("Select Serial Port:"))
-        vbox.addWidget(self.serial_combo)
-        vbox.addWidget(QLabel("Message:"))
+        vbox.addLayout(hbox_refresh)
+        vbox.addWidget(QLabel("Triggers:"))
         vbox.addWidget(self.message_box)
         vbox.addWidget(self.send_button)
         vbox.addWidget(self.slider_label)
         vbox.addWidget(self.slider)
         vbox.addWidget(QLabel("Sent Messages:"))
         vbox.addWidget(self.log_box)
+        vbox.addWidget(QLabel("© myBrainTechnologies - 2023"))
 
         self.setWindowTitle('MBT TriggerBox Tester')
         self.setLayout(vbox)
@@ -80,33 +102,38 @@ class SerialSender(QWidget):
     def refresh_serial_ports(self):
         self.serial_combo.clear()
         ports = [port.device for port in list_ports.comports()]
+        for port, desc, hwid in sorted(ports):
+            self.log_box.append("Found {}: {} [{}]".format(port, desc, hwid))
+            self.log_box.repaint()
         self.serial_combo.addItems(ports)
 
     def send_message(self):
-        if not self.serial_port or not self.serial_port.isOpen():
-            self.log_box.append("You need to select a serial port.")
+        message = self.message_box.toPlainText().replace('\n', '').strip()
+        if message == 'dev':
+            self.log_box.append("This tool was developed by Michele Romani.")
             self.log_box.repaint()
             return
-        message = self.message_box.toPlainText().replace('\n', '').strip()
         if not message:
-            self.log_box.append("Write comma separated characters in the text box to send them as triggers.")
+            self.log_box.append("Write space separated characters in the text box to send them as triggers.")
+            self.log_box.repaint()
+            return
+
+        if not self.serial_port or not self.serial_port.isOpen():
+            self.log_box.append("You need to select a serial port.")
             self.log_box.repaint()
             return
         message_bytes = message.encode('ascii')
         delay = self.slider.value()
         self.send_button.setEnabled(False)
-        self.log_box.append(message)
+        self.log_box.append("Sending: " + message + " with " + delay + "s intervals.")
         self.log_box.repaint()
-        if self.slider.value() == 1:
-            self.serial_port.write(message_bytes)
-        else:
-            for byte in message_bytes:
-                self.serial_port.write(bytes([byte]))
-                time.sleep(delay)
+        self.serial_port.write(message_bytes)
+        time.sleep(delay)
         self.send_button.setEnabled(True)
 
     def slider_value_changed(self, value):
         self.slider_label.setText("Delay: {} sec".format(value))
+
 
 
 if __name__ == '__main__':
